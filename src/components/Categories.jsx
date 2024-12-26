49a3599af1402e256e8e759f062ac18225e5c5ee
ilyaxtom <xtom38@gmail.com>
2024-12-27 00:24:15 +0300

    Created Header Pizza Sort components

---
+++ src/components/Categories.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Categories () {
+    const categories = ['Все', 'Мясные','Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+
+    const [activeCategory, setActiveCategory] = React.useState(0);
+
+    return (
+        <div className="categories">
+            <ul>
+                {
+                    categories.map((category, index) => (
+                        <li
+                            key={index}
+                            onClick={() => setActiveCategory(index)}
+                            className={activeCategory === index ? 'active' : ''}
+                        >{category}</li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default Categories;
